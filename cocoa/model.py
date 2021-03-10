627bee77d114dadefb6955d890091b9585131063
SaurusXI <vermashantanu@hotmail.com>
2021-03-10 11:32:30 +0530

    fix: add self references to modelservice methods

--- cocoa/model.py
+++ cocoa/model.py
@@ -38,55 +38,55 @@ class ModelService:
         self.session = sessionmaker(db)()
         base.metadata.create_all(db)
 
-    def add_meeting(uid1: int, uid2: int):
+    def add_meeting(self, uid1: int, uid2: int):
         meeting = Meeting(UID1 = uid1, UID2 = uid2)
         self.session.add(meeting)
         self.session.commit()
 
-    def delete_meeting(uid1: int, uid2: int):
+    def delete_meeting(self, uid1: int, uid2: int):
         meeting = self.session.query(Meeting).get((uid1, uid2))
         self.session.delete(meeting)
         self.session.commit()
 
-    def add_user(uid: int, description: str):
+    def add_user(self, uid: int, description: str):
         user = User(UID = uid, Description = description)
         self.session.add(user)
         self.session.commit()
 
-    def read_user(uid: int):
+    def read_user(self, uid: int):
         user = self.session.query(User).get(uid)
         return user
 
-    def update_user(uid: int, description: str):
+    def update_user(self, uid: int, description: str):
         user = read_user(uid)
         user.Description = description
         self.session.commit()
 
-    def delete_user(uid: int):
+    def delete_user(self, uid: int):
         user = read_user(uid)from datetime import time
         self.session.delete(user)
         self.session.commit()
 
-    def add_schedule(uid: int, start: time, end: time):
+    def add_schedule(self, uid: int, start: time, end: time):
         schedule = Schedule(UID = uid, StartTime = start, EndTime = end)
         self.session.add(schedule)
         self.session.commit()
 
-    def get_user_schedules(uid: int):
+    def get_user_schedules(self, uid: int):
         schedules = self.session.query(Schedule).filter(Schedule.UID == uid)
         return schedules
 
-    def read_schedule(uid: int, start: time, end: time):
+    def read_schedule(self, uid: int, start: time, end: time):
         schedule = self.session.query(Schedule).get((uid, start, end))
         return schedule
 
-    def update_schedule(uid: int, prevstart: time, prevend: time, newstart: time, newend: time):
+    def update_schedule(self, uid: int, prevstart: time, prevend: time, newstart: time, newend: time):
         schedule = read_schedule(uid, prevstart, prevend)
         schedule.StartTime = newstart
         schedule.EndTime = newend
         self.session.commit()
 
-    def delete_schedule(uid: int, start: time, end: time):
+    def delete_schedule(self, uid: int, start: time, end: time):
         schedule = read_schedule(uid, start, end)
         self.session.delete(schedule)
         self.session.commit()
