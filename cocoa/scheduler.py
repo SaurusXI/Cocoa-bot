3e57438728a48d0de4b927ce8a806c600fe8aae7
BALaka-18 <balaka2605@gmail.com>
2021-03-11 00:53:38 +0530

    Feat: Notifier service to notify users, Add: Call notify method in scheduler

--- cocoa/scheduler.py
+++ cocoa/scheduler.py
@@ -1,15 +1,18 @@
 from datetime import datetime, timedelta
 from model import ModelService
+from notifier import NotifService
+from discord import Client, TextChannel
 
 class Scheudler:
-    def __init__(self, modelsvc: ModelService, meeting_length: timedelta):
+    def __init__(self, modelsvc: ModelService, notifiersvc: NotifService, meeting_length: timedelta):
         self.meeting_length = meeting_length
         self.modelsvc = modelsvc
+        self.notifiersvc = notifiersvc
 
-    def schedule(self, uid: int, starttime: datetime, endtime: datetime):
+    def schedule(self, uid: int, starttime: datetime, endtime: datetime, channel: TextChannel, client: Client):
         self.modelsvc.add_schedule(uid, starttime, endtime)
 
         potential_meetings = self.modelsvc.find_meetings(starttime, endtime, self.meeting_length)
 
-        # To do - call notifier service and send notification of possible meetings
-        # self.notifiersvc.notify(potential_meetings, ...)
+        # Call notifier service and send notification of possible meetings
+        self.notifiersvc.notify(potential_meetings, channel, client)
