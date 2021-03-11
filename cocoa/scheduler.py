4fc2e150773e45d797b08718f4a3151f19c2d5c6
andreea.moraru <andreeamoraru090@gmail.com>
2021-03-11 10:41:18 +0100

    updated BookingService with a new function and fixed a small type in the Scheduler service
    Signed-off-by: andreea.moraru <andreeamoraru090@gmail.com>

--- cocoa/scheduler.py
+++ cocoa/scheduler.py
@@ -1,15 +1,20 @@
 from datetime import datetime, timedelta
+from notifier import NotifierService
 from model import ModelService
+from discord import Client, TextChannel
 
-class Scheudler:
-    def __init__(self, modelsvc: ModelService, meeting_length: timedelta):
+
+class Scheduler:
+    def __init__(self, modelsvc: ModelService, notifiersvc: NotifierService, meeting_length: timedelta):
         self.meeting_length = meeting_length
         self.modelsvc = modelsvc
+        self.notifiersvc = notifiersvc
 
-    def schedule(self, uid: int, starttime: datetime, endtime: datetime):
+    def schedule(self, uid: int, starttime: datetime, endtime: datetime, channel: TextChannel, client: Client):
         self.modelsvc.add_schedule(uid, starttime, endtime)
 
         potential_meetings = self.modelsvc.find_meetings(starttime, endtime, self.meeting_length)
 
-        # To do - call notifier service and send notification of possible meetings
-        # self.notifiersvc.notify(potential_meetings, ...)
+        if potential_meetings:
+            # Call notifier service and send notification of possible meetings
+            self.notifiersvc.notify(potential_meetings, channel, client)
