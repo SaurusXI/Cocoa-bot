e615e9ae47c09608152b0ac1f6a99097a3a34dae
SaurusXI <vermashantanu@hotmail.com>
2021-03-13 18:53:31 +0530

    fix: improve command handling

--- cocoa/controller.py
+++ cocoa/controller.py
@@ -18,19 +18,20 @@ class Controller:
             return
 
         _, _, command = message.content.partition(' ')
+        command = command.lower()
         author, channel = message.author, message.channel
 
         # User registration and related commands
         if command == 'setup':
             self.configsvc.register_user(author, channel, client)
-        elif command == 'delete_me':
+        elif command == 'delete me':
             self.configsvc.delete_user(author.id, channel)
         # Scheduling meetings and related commands
-        elif command == 'schedule_new':
+        elif command == 'schedule new':
             self.schedulersvc.schedule(author.id, channel, client),
-        elif command == 'schedule_cancel':
+        elif command == 'schedule cancel':
             self.schedulersvc.cancel(author.id, channel, client)
-        elif command == 'list_all_meetings':
+        elif command == 'list meetings':
             self.schedulersvc.list_booked_meetings(author.id, channel)
         else:
             await message.channel.send("Sorry, I'm not sure what you mean. Please use the help command for a list of commands you can use.")
